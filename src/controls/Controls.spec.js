7816087768700a52fe9609fdc7b0f9fe3dbc29c0
msearles25 <skaman4459@gmail.com>
2019-12-16 16:44:18 -0500

    display test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,5 @@
 // Test away!
+
+import React from 'react';
+
+import Controls from './Controls';
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,23 @@
 // Test away!
+
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+test('Correct is rendered base on locked state', () => {
+    const { getByTestId, rerender} = render(<Display locked/>);
+    getByTestId('locked')
+
+    rerender(<Display locked={true}/>)
+    getByTestId('locked')
+})
+
+test('Correct is rendered base on closed state', () => {
+    const { getByTestId, rerender} = render(<Display closed/>);
+    getByTestId('closed')
+
+    rerender(<Display closed={true}/>)
+    getByTestId('closed')
+})
+
