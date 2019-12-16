94b331e90fe6e073a7514c4783f601a2fa504ce0
msearles25 <skaman4459@gmail.com>
2019-12-16 15:51:09 -0500

    tested dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
 // Test away
+
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from '../display/Display';
+import Controls from '../controls/Controls';
+
+test('Renders display component', () => {
+    const displayTree = render(<Display />)
+    expect(displayTree).toMatchSnapshot()
+})
+
+test('Renders controls component', () => {
+    const controlsyTree = render(<Controls />)
+    expect(controlsyTree).toMatchSnapshot()
+})
