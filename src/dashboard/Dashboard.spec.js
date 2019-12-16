7649c3132d2800828a450a94f4245301ccc7e1c7
msearles25 <skaman4459@gmail.com>
2019-12-16 16:14:08 -0500

    a

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -6,12 +6,10 @@ import { render } from '@testing-library/react';
 import Display from '../display/Display';
 import Controls from '../controls/Controls';
 
-test('Renders display component', () => {
-    const displayTree = render(<Display />)
-    expect(displayTree).toMatchSnapshot()
+it('Renders display component', () => {
+   render(<Display />)
 })
 
-test('Renders controls component', () => {
-    const controlsyTree = render(<Controls />)
-    expect(controlsyTree).toMatchSnapshot()
+it('Renders controls component', () => {
+    render(<Controls />)
 })
